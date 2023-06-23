1e8bca3e12c7ec3f87d0186a491c0d84174d8420
RoomeyRahman <roomeyrahman@gmail.com>
2023-06-23 17:25:04 +0600

    add error handlar

--- packages/ranking.py
+++ packages/ranking.py
@@ -10,105 +10,140 @@ skill_corpus_file = "skill_corpus.pkl"
 
 class Ranker:
     def __init__(self):
-        self.word2vec = Word2Vec(min_count=20,
-            window=3,
-            vector_size=300,
-            sample=6e-5,
-            alpha=0.03,
-            min_alpha=0.0007,
-            negative=20
-        )
-        self.parser = CVParser(skill_corpus_file)
+        try:
+            self.word2vec = Word2Vec(min_count=20,
+                window=3,
+                vector_size=300,
+                sample=6e-5,
+                alpha=0.03,
+                min_alpha=0.0007,
+                negative=20
+            )
+            self.parser = CVParser(skill_corpus_file)
+        except Exception as e:
+            print("Error occurred while initializing the Ranker:", str(e))
 
     def z_score_normalization(self, scores):
-        mean = np.mean(scores)
-        std = np.std(scores)
-        normalized_scores = (scores - mean) / std
-        return normalized_scores
+        try:
+            mean = np.mean(scores)
+            std = np.std(scores)
+            normalized_scores = (scores - mean) / std
+            return normalized_scores
+        except Exception as e:
+            print("Error occurred during z-score normalization:", str(e))
+            return None
 
     def rank_keyword(self, cv, keyword):
-        match_keyword = self.parser.extract_keyword(cv, keyword)
-        rank = (len(match_keyword) / len(keyword)) * 100
-        return round(rank, 2)
+        try:
+            match_keyword = self.parser.extract_keyword(cv, keyword)
+            rank = (len(match_keyword) / len(keyword)) * 100
+            return round(rank, 2)
+        except Exception as e:
+            print("Error occurred during keyword ranking:", str(e))
+            return None
 
     def rank_jaccard_keyword(self, cv, keyword):
-        # Preprocess the documents
-        cv = cv.lower().strip()
-        keyword = [x.lower() for x in keyword]
-        cv_words = set(cv.split())
-        keyword_words = set(keyword)
+        try:
+            # Preprocess the documents
+            cv = cv.lower().strip()
+            keyword = [x.lower() for x in keyword]
+            cv_words = set(cv.split())
+            keyword_words = set(keyword)
 
-        # Calculate the intersection and union of word sets
-        intersection = cv_words.intersection(keyword_words)
-        union = cv_words.union(keyword_words)
+            # Calculate the intersection and union of word sets
+            intersection = cv_words.intersection(keyword_words)
+            union = cv_words.union(keyword_words)
 
-        # Calculate Jaccard similarity
-        similarity = len(intersection) / len(union)
-        return similarity
+            # Calculate Jaccard similarity
+            similarity = len(intersection) / len(union)
+            return similarity
+        except Exception as e:
+            print("Error occurred during Jaccard keyword ranking:", str(e))
+            return None
 
     def rank_jaccard(self, jd, cv):
-        # Preprocess the documents
-        jd = jd.lower().strip()
-        cv = cv.lower().strip()
-        jd_words = set(jd.split())
-        cv_words = set(cv.split())
+        try:
+            # Preprocess the documents
+            jd = jd.lower().strip()
+            cv = cv.lower().strip()
+            jd_words = set(jd.split())
+            cv_words = set(cv.split())
 
-        # Calculate the intersection and union of word sets
-        intersection = jd_words.intersection(cv_words)
-        union = jd_words.union(cv_words)
+            # Calculate the intersection and union of word sets
+            intersection = jd_words.intersection(cv_words)
+            union = jd_words.union(cv_words)
 
-        # Calculate Jaccard similarity
-        similarity = len(intersection) / len(union)
-        return similarity
+            # Calculate Jaccard similarity
+            similarity = len(intersection) / len(union)
+            return similarity
+        except Exception as e:
+            print("Error occurred during Jaccard ranking:", str(e))
+            return None
 
     def rank_cosine(self, jd, cv):
-        Match_Test = [jd, cv]
-        vector = TfidfVectorizer(
-            ngram_range=(1, 3),                 # Include both unigrams and bigrams
-            sublinear_tf=True,                  # Use sublinear term frequency scaling
-            max_features=1000                   # Set the maximum number of features
-        )
-        count_matrix = vector.fit_transform(Match_Test)
-        MatchPercentage = cosine_similarity(count_matrix)[0][1] * 100
-        MatchPercentage = round(MatchPercentage, 2)
-        return MatchPercentage
+        try:
+            Match_Test = [jd, cv]
+            vector = TfidfVectorizer(
+                ngram_range=(1, 3),                 # Include both unigrams and bigrams
+                sublinear_tf=True,                  # Use sublinear term frequency scaling
+                max_features=1000                   # Set the maximum number of features
+            )
+            count_matrix = vector.fit_transform(Match_Test)
+            MatchPercentage = cosine_similarity(count_matrix)[0][1] * 100
+            MatchPercentage = round(MatchPercentage, 2)
+            return MatchPercentage
+        except Exception as e:
+            print("Error occurred during cosine ranking:", str(e))
+            return None
 
     def rank_wmd(self, jd, cv):
-        # Prepare documents as lists of words
-        jd_doc = jd.split()
-        cv_doc = cv.split()
+        try:
+            # Prepare documents as lists of words
+            jd_doc = jd.split()
+            cv_doc = cv.split()
 
-        # Build Word2Vec models for the documents
-        model = Word2Vec(
-            [jd_doc, cv_doc],
-            min_count=1,
-            window=3,
-            vector_size=300,
-            sample=6e-5,
-            alpha=0.03,
-            min_alpha=0.0007,
-            negative=20
-        )
+            # Build Word2Vec models for the documents
+            model = Word2Vec(
+                [jd_doc, cv_doc],
+                min_count=1,
+                window=3,
+                vector_size=300,
+                sample=6e-5,
+                alpha=0.03,
+                min_alpha=0.0007,
+                negative=20
+            )
 
-        # Calculate the WMD
-        distance = model.wv.wmdistance(jd_doc, cv_doc, lambda x, y: model.wv.distance(x, y))
+            # Calculate the WMD
+            distance = model.wv.wmdistance(jd_doc, cv_doc, lambda x, y: model.wv.distance(x, y))
 
-        # Convert distance to match percentage
-        match_percentage = round(100 - distance, 2)
-        return match_percentage
+            # Convert distance to match percentage
+            match_percentage = round(100 - distance, 2)
+            return match_percentage
+        except Exception as e:
+            print("Error occurred during WMD ranking:", str(e))
+            return None
 
     def rank_bert(self, jd, cv):
-        # Calculate cosine similarity between embeddings
-        similarity = cosine_similarity(jd, cv)[0][0]
-        match_percentage = round(similarity * 100, 2)
-        return match_percentage
+        try:
+            # Calculate cosine similarity between embeddings
+            similarity = cosine_similarity(jd, cv)[0][0]
+            match_percentage = round(similarity * 100, 2)
+            return match_percentage
+        except Exception as e:
+            print("Error occurred during BERT ranking:", str(e))
+            return None
 
     def rank_doc2vec(self, jd, cv):
-        model = Doc2Vec.load('doc2vec.model')
-        v1 = model.infer_vector(cv.split())
-        v2 = model.infer_vector(jd.split())
-        cosine_similarity = (np.dot(np.array(v1), np.array(v2))) / (norm(np.array(v1)) * norm(np.array(v2))) * 100
-        return round(float(cosine_similarity), 2)
+        try:
+            model = Doc2Vec.load('doc2vec.model')
+            v1 = model.infer_vector(cv.split())
+            v2 = model.infer_vector(jd.split())
+            cosine_similarity = (np.dot(np.array(v1), np.array(v2))) / (norm(np.array(v1)) * norm(np.array(v2))) * 100
+            return round(float(cosine_similarity), 2)
+        except Exception as e:
+            print("Error occurred during Doc2Vec ranking:", str(e))
+            return None
 
     def rank_combined(
             self,
@@ -120,12 +155,16 @@ class Ranker:
             bert_weight=0.25,
             doc2vec_weight=0.25):
 
-        # Combine cosine score and keyword score with weights
-        combined_cosine_keyword = (0.25 * cosine_score) + (0.75 * keyword_score)
+        try:
+            # Combine cosine score and keyword score with weights
+            combined_cosine_keyword = (0.25 * cosine_score) + (0.75 * keyword_score)
 
-        # Combine BERT score and WMD score with weights
-        combined_bert_wmd = (0.90 * bert_score) + (0.10 * wmd_score)
+            # Combine BERT score and WMD score with weights
+            combined_bert_wmd = (0.90 * bert_score) + (0.10 * wmd_score)
 
-        combined_score = (combined_bert_wmd * bert_weight) + (doc2vec_score * doc2vec_weight) + ((1 - (bert_weight+doc2vec_weight)) * combined_cosine_keyword)
-        combined_score = round(combined_score, 2)
-        return combined_score
+            combined_score = (combined_bert_wmd * bert_weight) + (doc2vec_score * doc2vec_weight) + ((1 - (bert_weight+doc2vec_weight)) * combined_cosine_keyword)
+            combined_score = round(combined_score, 2)
+            return combined_score
+        except Exception as e:
+            print("Error occurred during combined ranking:", str(e))
+            return None
